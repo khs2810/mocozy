03ce9fa75207f0daa601d714ab63f805eacc185a
DrinkUpWater <ciwdj90@gmail.com>
2024-05-21 12:02:45 +0900

    [teo] 데이터베이스와 연동 테스트 성공 루트, 맵퍼, member club vo 생성

--- java/com/kh/mocozy/club/model/dao/ClubDao.java
+++ java/com/kh/mocozy/club/model/dao/ClubDao.java
@@ -1,8 +1,13 @@
 package com.kh.mocozy.club.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ClubDao {
+
+	public int increaseCount(SqlSessionTemplate sqlSession, int cno) {
+		return sqlSession.update("clubMapper.increaseCount", cno);
+	}
 
 }
--- java/com/kh/mocozy/club/service/ClubService.java
+++ java/com/kh/mocozy/club/service/ClubService.java
@@ -1,5 +1,8 @@
 package com.kh.mocozy.club.service;
 
 public interface ClubService {
+	
+	//클럽 조회수 증가
+	int increaseCount(int cno);
 
 }
