0216aacd7ff5a84afe2fd4d313399b08d43df61a
Gyummo <johndoe@example.com>
2024-05-30 21:32:32 +0900

    [조현지] 헤더 수정

--- webapp/WEB-INF/views/categories/cateReview.jsp
+++ webapp/WEB-INF/views/categories/cateReview.jsp
@@ -48,7 +48,7 @@
 									<c:forEach var="club" items="${catelist}">
 										<div class="contentcard">
 											<div class="socialing">
-												<a class="cardlink" href="detail.cl?cno=1">
+												<a class="cardlink" href="detail.cl?cno=${club.clubNo}">
 													<img class="img" src="${club.thumbnailImg}" />
 
 													<div class="info">
--- webapp/resources/jo/js/common_js/header.js
+++ webapp/resources/jo/js/common_js/header.js
@@ -1,37 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
-  /* 메뉴 토글*/
-  const TogglemenuButton = document.querySelector('#cateButton');
-  const TogglemenuItems = document.querySelector('#cateToggle');
-
-  /* 메뉴 버튼 클릭시 카테고리 토글*/
-  TogglemenuButton.addEventListener('click', function() {
-  /* css display 속성 가져오기 */	
-  const Toggledisplay = TogglemenuItems.style.display;
-    if (Toggledisplay === 'none') {
-      TogglemenuItems.style.display = 'block';
-    } else {
-      TogglemenuItems.style.display = 'none';
-    }
-  });
-
-
-  /* 로그인 토글*/
-  const ToggleLoginButton = document.querySelector('#userName');
-  const ToggleLoginItems = document.querySelector('#LoginToggle');
-
-  ToggleLoginButton.addEventListener('click', function() {
-  console.log("1");
-  /* css display 속성 가져오기 */
-  const ToggleLogindisplay = ToggleLoginItems.style.display;
-    if (ToggleLogindisplay === 'none') {
-      ToggleLoginItems.style.display = 'block';
-
-      /* 클릭 이벤트시 클릭한 위치를 좌표 설정 */
-      ToggleLoginItems.style.position = 'fixed';
-      ToggleLoginItems.style.left = event.clientX + 'px';
-      ToggleLoginItems.style.top = event.clientY + 'px';
-    } else {
-      ToggleLoginItems.style.display = 'none';
-    }
-  });
-});
+// document.addEventListener('DOMContentLoaded', function(){
+//     /* 메뉴 토글*/
+//     const TogglemenuButton = document.querySelector('#cateButton');
+//     const TogglemenuItems = document.querySelector('#cateToggle');
+  
+//     /* 메뉴 버튼 클릭시 카테고리 토글*/
+//     TogglemenuButton.addEventListener('click', function() {
+//     const Toggledisplay = window.getComputedStyle(TogglemenuItems).height;
+//     if (Toggledisplay === '10px') {
+//       TogglemenuItems.style.backgroundColor = 'white';
+//       TogglemenuItems.style.height = '200px';
+//     } else {
+//       TogglemenuItems.style.backgroundColor = 'white';
+//       TogglemenuItems.style.height = '10px';
+//     }
+//     });
+// });
