79a3c7e797d6057dfec0a3486f56f80599885ee6
ByeongJun99 <jo__y@naver.com>
2024-06-03 17:29:56 +0900

    [임병준] 내가 만든 소셜링 페이지 구현 완료, 소셜링 종료 취소기능 구현

--- java/com/kh/mocozy/member/controller/SocialingController.java
+++ java/com/kh/mocozy/member/controller/SocialingController.java
@@ -9,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
 import com.kh.mocozy.club.model.vo.Club;
 import com.kh.mocozy.club.service.ClubService;
 import com.kh.mocozy.member.model.vo.Member;
@@ -28,6 +31,7 @@ public class SocialingController {
     public String mySocialView(Model model) {
         int uno = 2;
         ArrayList<Club> clist = clubService.selectMySocialList(uno);
+        ArrayList<Club> dlist = clubService.selectMySocialListDone(uno);
 
         for (int i = 0; i < clist.size(); i++) {
             ArrayList<Member> memberList = memberService.participatedMemberList(clist.get(i).getClubNo());
@@ -44,9 +48,78 @@ public class SocialingController {
         }
 
         model.addAttribute("clist", clist);
+        model.addAttribute("dlist", dlist);
 
         return "myPage/mySocial";
     }
+    
+    // 소셜링 종료
+	@RequestMapping("finishSocial.me")
+    public String finishSocial(int cno, Model model) {
+    	int result = clubService.finishSocial(cno);
+
+    	if (result > 0) {
+    		int uno = 2;
+            ArrayList<Club> clist = clubService.selectMySocialList(uno);
+            ArrayList<Club> dlist = clubService.selectMySocialListDone(uno);
+
+            for (int i = 0; i < clist.size(); i++) {
+                ArrayList<Member> memberList = memberService.participatedMemberList(clist.get(i).getClubNo());
+                clist.get(i).setMemberCnt(memberList.size());
+
+                // Timestamp를 LocalDateTime으로 변환
+                LocalDateTime dateTime = clist.get(i).getEventDate().toLocalDateTime();
+
+                // 밀리초 부분 제거
+                dateTime = dateTime.withNano(0);
+
+                // LocalDateTime을 Timestamp로 변환하여 저장
+                clist.get(i).setEventDate(Timestamp.valueOf(dateTime));
+            }
+
+            model.addAttribute("clist", clist);
+            model.addAttribute("dlist", dlist);
+
+            return "myPage/mySocial";
+    	} else {
+    		model.addAttribute("errorMsg", "소셜링 종료 실패");
+			return "common/errorPage";
+    	}
+    }
+	
+	// 소셜링 종료취소
+	@RequestMapping("cancleFinishSocial.me")
+	public String cancleFinishSocial(int cno, Model model) {
+		int result = clubService.cancleFinishSocial(cno);
+		
+		if (result > 0) {
+    		int uno = 2;
+            ArrayList<Club> clist = clubService.selectMySocialList(uno);
+            ArrayList<Club> dlist = clubService.selectMySocialListDone(uno);
+
+            for (int i = 0; i < clist.size(); i++) {
+                ArrayList<Member> memberList = memberService.participatedMemberList(clist.get(i).getClubNo());
+                clist.get(i).setMemberCnt(memberList.size());
+
+                // Timestamp를 LocalDateTime으로 변환
+                LocalDateTime dateTime = clist.get(i).getEventDate().toLocalDateTime();
+
+                // 밀리초 부분 제거
+                dateTime = dateTime.withNano(0);
+
+                // LocalDateTime을 Timestamp로 변환하여 저장
+                clist.get(i).setEventDate(Timestamp.valueOf(dateTime));
+            }
+
+            model.addAttribute("clist", clist);
+            model.addAttribute("dlist", dlist);
+
+            return "myPage/mySocial";
+    	} else {
+    		model.addAttribute("errorMsg", "소셜링 종료 취소 실패");
+			return "common/errorPage";
+    	}
+	}
 
     // 참가한 소셜
     @RequestMapping("goSocial.me")
